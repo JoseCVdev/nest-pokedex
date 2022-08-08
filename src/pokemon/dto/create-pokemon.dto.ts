8e0e57c1a1ebd1ff7a768a1bda6264d445570512
JoseCVdev <chacpzjose@gmail.com>
2022-08-08 14:22:57 -0500

    api-poke

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,15 @@
+import { IsInt, IsPositive, IsString, MinLength, Min } from "class-validator";
+
+
+export class CreatePokemonDto {
+    
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+
+}
